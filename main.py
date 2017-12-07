9a27596cf8cb78ec74a31b363fc9973c1099ddc0
Brian Coveney <coveneybrian@gmail.com>
2017-12-07 21:00:18 +0000

    Fix random numbers

--- main.py
+++ main.py
@@ -1,46 +1,72 @@
 import random
 
-# ------------------------------------------
-# Global Variables
-# ------------------------------------------
-board_size = 8
-num_queens = 8
 
+# ------------------------------------------
+# Returns the heuristic value of our initial board state
+# - outer loop is the rows in the range of our random
+# ------------------------------------------
+def getHeuristic(random_list):
+    heuristic = 0
 
-def get_h_cost(board):
-    h = 0
-    for i in range(len(board)):
-        # Check every column we haven't already checked
-        for j in range(i + 1, len(board)):
-            # Queens are in the same row
-            if board[i] == board[j]:
-                h += 1
+    for row in range(len(random_list)):
+        for col in range(row + 1, len(random_list)):
+            # Increment our heuristic if there are Queens are in the same row
+            if random_list[row] == random_list[col]:
+                heuristic += 1
             # Get the difference between the current column
             # and the check column
-            offset = j - i
+            offset = col - row
             # To be a diagonal, the check column value has to be
             # equal to the current column value +/- the offset
-            if board[i] == board[j] - offset or board[i] == board[j] + offset:
-                h += 1
+            if random_list[row] == random_list[col] - offset  or random_list[row] == random_list[col] + offset:
+                heuristic += 1
 
-    return h
+    return heuristic
 
 
-def generateRandomNumbers():
-    numbers = []
-    for n in range(num_queens):
-        numbers.append(n)
-    random.shuffle(numbers)
+def make_move_first_choice(board):
+    h_to_beat = getHeuristic(board)
+    for col in range(len(board)):
+        for row in range(len(board)):
+            if board[col] == row:
+                # We don't need to evaluate the current
+                # position, we already know the h-value
+                continue
+
+            board_copy = list(board)
+            # Move the queen to the new row
+            board_copy[col] = row
+            new_h_cost = getHeuristic(board_copy)
+
+            # Return the first better (not best!) match you find
+            if new_h_cost < h_to_beat:
+                board[col] = row
+                return board
+
+    return board
+
+
+# ------------------------------------------
+# Return as list of random numbers
+# ------------------------------------------
+def getRandomNumbers():
+    numbers = [3, 4, 7, 5, 0, 2, 1, 6]
     return numbers
 
 
-def displayBoard():
-    for row in range(board_size):
+# ------------------------------------------
+# Prints the chess board with:
+#  - positions of 'Q's retrieved from the index of our random number list
+#  - otherwise positions are left blank
+# ------------------------------------------
+def displayBoard(num_queens):
+
+    for row in range(num_queens):
         print("", end="|")
 
-        queen = generateRandomNumbers().index(row)
+        queen = getRandomNumbers().index(row)
 
-        for col in range(board_size):
+        for col in range(num_queens):
             if col == queen:
                 print("Q", end="|")
             else:
@@ -48,19 +74,26 @@ def displayBoard():
         print("")
 
 
+# ------------------------------------------
+# Returns the selected number of queens
+# Allows for changing the size of the board
+# ------------------------------------------
+def getNumQueens(count):
+    return count
+
+
 def main():
+    num_queens = getNumQueens(8)
 
-    # a predetermined layout
-    # queens = list(range(board_size))
+    random_list = getRandomNumbers()
+    print("Random Initial State:", random_list)
 
-    board = generateRandomNumbers()
+    heuristic = getHeuristic(random_list)
+    print("Heuristic Value:", heuristic)
 
-    heu = get_h_cost(board)
-    print(heu)
-
-    displayBoard()
-    print(board)
-
+    # first_choice_hc = make_move_first_choice(random_list)
+    # print(first_choice_hc)
+    displayBoard(num_queens)
 
 if __name__ == '__main__':
     main()
