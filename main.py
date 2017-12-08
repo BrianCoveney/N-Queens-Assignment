ff8ae162fbd0167686d1c228ae7178d770079550
BrianCoveney <coveneybrian@gmail.com>
2017-12-08 20:05:19 +0000

    Make cosmetic changes

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ N = 8
 
 
 # ------------------------------------------
-# Returns the heuristic value of our initial board state
+# Return the heuristic value of our initial board state
 # ------------------------------------------
 def getHeuristic(board):
     heuristic = 0
@@ -25,7 +25,7 @@ def getHeuristic(board):
 
 
 # ------------------------------------------
-# Return as list of random numbers
+# Return a list of random numbers
 # ------------------------------------------
 def getRandomNumbers(num_queens):
     numbers = []
@@ -45,7 +45,7 @@ def moveOneQueen(board):
             board_copy[row], board_copy[col] = board_copy[col], board_copy[row]
             new_heuristic = getHeuristic(board_copy)
 
-            # Return the first better (not best!) match you find
+            # Return the first better match
             if new_heuristic < initial_heuristic:
                 board[col] = board[row]
                 return board
@@ -101,54 +101,51 @@ def displayBoard(board):
         print("")
 
 
-def randomRestartHillClimb(steepest_hill, steepest_hill_heu):
+def randomRestartHillClimb(old_steepest_hill, old_steepest_hill_heu):
     restarts = 500
     restart_count = 0
     count_new_solution = 0
     count_old_solution = 0
     best_solution = []
-    total_number_of_moves = 0
 
+    # Initially set the maximum number of restarts to 500
     while restart_count < restarts:
+        # Generate a new random configuration for the initial state of the board
         new_random = getRandomNumbers(N)
         new_steepest_hill = steepestHill(new_random)
         new_steepest_hill_heu = getHeuristic(new_steepest_hill)
-
         print("")
 
+        # A solution was found for our new random state. Print details and exit.
         if new_steepest_hill_heu == 0:
-            print("Correct Answer found in New Steepest Hill count: ", count_new_solution, "\n",
-                  new_steepest_hill, sep="")
-
+            print("Correct Answer found in New Steepest Hill count: ",count_new_solution,"\n", new_steepest_hill,sep="")
             best_solution = new_steepest_hill
-            displayBoard(best_solution)
             return best_solution
 
-        elif steepest_hill_heu == 0:
-            print("Correct Answer found in Old Steepest Hill count: ", count_old_solution, "\n",
-                  steepest_hill, sep="")
-
-            best_solution = steepest_hill
-            displayBoard(best_solution)
+        # A solution was found for out steepest ascent algorithm. Print details and exit.
+        elif old_steepest_hill_heu == 0:
+            print("Correct Answer found in Old Steepest Hill count: ",count_old_solution,"\n",old_steepest_hill,sep="")
+            best_solution = old_steepest_hill
             return best_solution
 
+        # Otherwise, print which is the better algorithm
         else:
-            if new_steepest_hill_heu < steepest_hill_heu:
+            if new_steepest_hill_heu < old_steepest_hill_heu:
                 print("Count New:",count_new_solution)
-                print("New solution better  ", steepest_hill)
+                print("New solution better  ", old_steepest_hill)
                 print("Heuristic Value:", new_steepest_hill_heu)
                 count_new_solution = count_new_solution + 1
             else:
                 print("Count Old:", count_old_solution)
                 print("Old solution better  ", new_steepest_hill)
-                print("Heuristic Value:", steepest_hill_heu)
+                print("Heuristic Value:", old_steepest_hill_heu)
                 count_old_solution = count_old_solution + 1
 
         restart_count = restart_count + 1
 
+        # Display total number of moves
         total_number_of_moves = count_old_solution + count_new_solution
         print("\nTotal number of moves:", total_number_of_moves)
-
 
     return best_solution
 
@@ -180,7 +177,8 @@ def main():
     print("------------------------------------------")
 
     # Random restart hill climbing
-    randomRestartHillClimb(steepest_hill, steepest_hill_heu)
+    solution = randomRestartHillClimb(steepest_hill, steepest_hill_heu)
+    displayBoard(solution)
 
 
 if __name__ == '__main__':
