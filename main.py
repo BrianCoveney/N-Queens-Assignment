f2a5cde7335a9926363062584cb80b8cdc576544
BrianCoveney <coveneybrian@gmail.com>
2017-12-08 19:11:15 +0000

    Break on RR Hill climb when solution is found

--- main.py
+++ main.py
@@ -103,29 +103,54 @@ def displayBoard(board):
 
 def randomRestartHillClimb(steepest_hill, steepest_hill_heu):
     restarts = 500
-    count = 0
+    restart_count = 0
     count_new_solution = 0
     count_old_solution = 0
 
-    while count < restarts:
+    while restart_count < restarts:
         new_random = getRandomNumbers(N)
         new_steepest_hill = steepestHill(new_random)
-        print("\nNew Steepest hill:   ", new_steepest_hill)
         new_steepest_hill_heu = getHeuristic(new_steepest_hill)
-        print("Heuristic Value:", new_steepest_hill_heu)
 
         print("")
 
-        if new_steepest_hill_heu < steepest_hill_heu:
-            print("New solution better  ", steepest_hill)
-            count_new_solution = count_new_solution + 1
+        best = []
+        if new_steepest_hill_heu == 0:
+            print("Correct Answer found in New Steepest Hill, at count: ", count_new_solution, "\n",
+                  new_steepest_hill, sep="")
+
+            best = new_steepest_hill
+            displayBoard(best)
+            return best
+
+        elif steepest_hill_heu == 0:
+            print("Correct Answer found in Old Steepest Hill, at count: ", count_old_solution, "\n",
+                  steepest_hill, sep="")
+
+            best = steepest_hill
+            displayBoard(best)
+            return best
+
         else:
-            print("Old solution better  ", new_steepest_hill)
-            count_old_solution = count_old_solution + 1
-        count = count + 1
+            if new_steepest_hill_heu < steepest_hill_heu:
+                print("Count New:",count_new_solution)
+                print("New solution better  ", steepest_hill)
+                print("Heuristic Value:", new_steepest_hill_heu)
+                count_new_solution = count_new_solution + 1
+            else:
+                print("Count Old:", count_old_solution)
+                print("Old solution better  ", new_steepest_hill)
+                print("Heuristic Value:", steepest_hill_heu)
+                count_old_solution = count_old_solution + 1
+
+        restart_count = restart_count + 1
+
 
     print("\nCount for new solution:", count_new_solution)
     print("Count for old solution:", count_old_solution)
+
+
+    return best
 
 
 def main():
